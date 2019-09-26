0397692e6494afceb38ce6e8b6fc4670fe904546
Daniel Rundle <derundle@ucdavis.edu>
2019-09-26 14:37:18 -0700

    Commented code; only accepts excel files; csv to come

--- src/app/read-spreadsheet.service.ts
+++ src/app/read-spreadsheet.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
+import { read } from 'fs';
 
 @Injectable({
     providedIn: 'root'
@@ -9,9 +10,16 @@ export class ReadSpreadsheetService{
 
     constructor() { }
 
-    buildMSPString(dataArray: any) {
-        var spectrum: string[];
+    // Create a string from a 2x2 array of MS/MS data
+    buildMspStringFromJson(dataArray: any) {
+        // Initialize string to be returned
         var mspString: string = "";
+
+        // A list of mass and intensity for each peak
+        var spectrum: string[];
+
+        // Traverse each row of dataArray and build mspString
+        //  Each row represents data for one metabolite
         dataArray.forEach((element:any) => {
             mspString += 
             "Name: " + element["Metabolite name"] + "\n" +
@@ -20,6 +28,8 @@ export class ReadSpreadsheetService{
             "Precursor Mz: " + element["Average Mz"] + "\n" +
             "Retention Time: " + element["Average Rt(min)"] + "\n" +
             "Formula: " + element["Formula"] + "\n";
+
+            // Create array of mass/intensity peaks to be written into the string line by line
             spectrum = element["MS/MS spectrum"].split(" ");
             mspString += "Num Peaks: " + spectrum.length.toString() + "\n";
             spectrum.forEach(massIntensity => {
@@ -28,22 +38,33 @@ export class ReadSpreadsheetService{
             mspString = mspString + "\n\n";
         });
         return mspString;
-    }
+
+    } // end buildMspStringFromJson
+
 
     mspFromCsv() {
 
-    }
+    } // end mspFromCsv
 
+
+    // Create .msp from an excel file
     mspFromXlsx(sheetData: FileList) {
 
         var reader = new FileReader();
 
+        // Create callback function for when the excel file has been loaded by the FileReader()
         reader.addEventListener('load', (loadEvent) => {
 
-            // Explicit type declaration so that Angular won't throw an error
+            // <FileReader> - explicit type declaration so that Angular won't throw an error
             var target: FileReader = <FileReader>loadEvent.target;
             var wb: XLSX.WorkBook = XLSX.read(target.result, { type: 'binary' });
+
+            // Convert spreadsheet data to JSON data
+            // Using header:1 will generate an array of arrays that we will traverse to find column headers
             var msmsArray: any[] = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]],{header:1});
+
+            // In the test spreadsheet, the column names weren't the top line; find them and store them
+            //  as column headers
             var headers: string[];
             var i: number;
             for (i = 0; i < msmsArray.length; i++) {
@@ -52,21 +73,29 @@ export class ReadSpreadsheetService{
                     break;
                 }
             }
-            // !ref means default
+
+            // Exclude any empty rows at the beginning of the spreadsheet by resetting its range
+            // !ref accesses the default range of the spreadsheet
             var range = XLSX.utils.decode_range(wb.Sheets[wb.SheetNames[0]]['!ref']);
             range.s.r = i + 1;
             var newRange = XLSX.utils.encode_range(range);
+
+            // Create a new array of arrays with the proper column headers and range
             var msmsArrayHeaders = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]],{header:headers, range:newRange});
 
-            var blob = new Blob([this.buildMSPString(msmsArrayHeaders)], {type: "text/plain;charset=utf-8"});
-            // Why does this line remove all console logs? What does saveAs do here?
+            // Create a string of the data to be written into a .msp file using buildMspStringFromJson()
+            //  Create a blob from the resulting string
+            var blob = new Blob([this.buildMspStringFromJson(msmsArrayHeaders)], {type: "text/plain;charset=utf-8"});
+            
+            // User will be prompted to save a .msp for their data
             saveAs(blob, sheetData[0].name.split(".")[0] + ".msp");
         });
       
         if (sheetData[0]) {
+            // Read the excel file and execute callback function from addEventListener
             reader.readAsBinaryString(sheetData[0]);
         }
 
-    }
+    } // end mspFromXlsx
   
 }
--- src/app/read-spreadsheet/read-spreadsheet.component.ts
+++ src/app/read-spreadsheet/read-spreadsheet.component.ts
@@ -17,23 +17,34 @@ export class ReadSpreadsheetComponent implements OnInit{
     constructor(private readSpreadsheetService: ReadSpreadsheetService) {}   
 
     ngOnInit() {
+        // Submit button disabled
         this.submitValid = false;
+
         this.errorText = "";
     }
 
+    // Called when user selects spreadsheet to be turned into a .msp
     fileSelected(changeEvent: Event) {
         var target = <HTMLInputElement>changeEvent.target;
+
+        // Submit button can now be clicked
         this.submitValid = true;
+        // Store selected file
         this.files = target.files;
     }
 
+    // Called when the user submits their spreadsheet
     readFile() {
-
+        // If the user has chosen a file, create .msp with ReadSpreadsheetService
+        // Otherwise, throw an error
         if (this.files) {
 
+            // Process either excel or csv spreadsheet
             var nameElements = this.files[0].name.split(".");
             if (nameElements[1] == "xlsx") this.readSpreadsheetService.mspFromXlsx(this.files);
             else if (nameElements[1] == "csv") this.readSpreadsheetService.mspFromCsv();
+
+            //Disable the Submit button
             this.submitValid = false;
             
         } else {
