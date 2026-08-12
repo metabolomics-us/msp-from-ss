d4c2fe859f1e0569673ac3852fb40fde42532758
Diego Pedrosa <linuxmant@gmail.com>
2026-08-12 13:31:22 -0700

    chore: upgrade to Angular 11

--- src/app/read-spreadsheet/read-spreadsheet.component.spec.ts
+++ src/app/read-spreadsheet/read-spreadsheet.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 // Doesn't seem to fix problem of Template parse errors
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ReadSpreadsheetComponent } from './read-spreadsheet.component';
@@ -8,7 +8,7 @@ describe('ReadSpreadsheetComponent', () => {
 	let component: ReadSpreadsheetComponent;
 	let fixture: ComponentFixture<ReadSpreadsheetComponent>;
 
-	beforeEach(async(() => {
+	beforeEach(waitForAsync(() => {
 		TestBed.configureTestingModule({
         declarations: [ ReadSpreadsheetComponent ],
         // imports: [FormsModule],
