5de6e5f9130f13ae19d562c59df1478435a9af82
Diego Pedrosa <linuxmant@gmail.com>
2026-08-18 16:15:50 -0700

    refactor: bootstrap via bootstrapApplication instead of NgModule

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideZoneChangeDetection(),
+    provideAnimations(),
+    provideRouter([])
+  ]
+}).catch(err => console.error(err));
